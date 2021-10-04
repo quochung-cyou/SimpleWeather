e0132035cb3e27c7c0fec8f93b52d98a37905535
quochungbn <nguyenquochung.6203@gmail.com>
2021-10-04 14:37:56 +0700

    Add SharedPreferences to store weather data for faster loading app

--- app/src/main/java/com/quochung/minimalweather/MainActivity.java
+++ app/src/main/java/com/quochung/minimalweather/MainActivity.java
@@ -1,6 +1,7 @@
 package com.quochung.minimalweather;
 
 import android.Manifest;
+import android.content.SharedPreferences;
 import android.content.pm.PackageManager;
 import android.graphics.Color;
 import android.graphics.LinearGradient;
@@ -8,7 +9,6 @@ import android.graphics.Shader;
 import android.location.Location;
 import android.os.Bundle;
 import android.text.TextPaint;
-import android.util.Log;
 import android.view.View;
 import android.widget.ImageView;
 import android.widget.TextView;
@@ -21,18 +21,12 @@ import androidx.core.content.ContextCompat;
 
 import com.android.volley.Request;
 import com.android.volley.RequestQueue;
-import com.android.volley.Response;
-import com.android.volley.VolleyError;
 import com.android.volley.toolbox.JsonArrayRequest;
 import com.android.volley.toolbox.JsonObjectRequest;
 import com.google.android.gms.location.FusedLocationProviderClient;
 import com.google.android.gms.location.LocationServices;
-import com.google.android.gms.tasks.OnCompleteListener;
-import com.google.android.gms.tasks.Task;
 
-import org.json.JSONArray;
 import org.json.JSONException;
-import org.json.JSONObject;
 
 import java.text.DateFormat;
 import java.util.Date;
@@ -44,8 +38,8 @@ public class MainActivity extends AppCompatActivity {
 
     TextView city, temp, state, date;
     ImageView weather, weatherblur;
-
-
+    SharedPreferences weatherdata;
+    SharedPreferences.Editor editor;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -65,6 +59,73 @@ public class MainActivity extends AppCompatActivity {
         Date currentdate = new Date();
         String stringDate = DateFormat.getDateInstance() .format(currentdate);
         date.setText(stringDate);
+        weatherdata = getSharedPreferences("weatherdata", MODE_PRIVATE);
+
+        state.setText(weatherdata.getString("statedata", "Clear"));
+        String coloruse  = weatherdata.getString("coloruse", "#8EC1DD");
+
+
+        //-------------------
+        ////Set cache temp
+        String temptext = weatherdata.getString("tempdata", "26") + "°";
+        temp.setText(temptext);
+        TextPaint paint = temp.getPaint();
+        float width = paint.measureText(temptext);
+
+        Shader textShader = new LinearGradient(10, 10, width, temp.getTextSize(),
+                new int[]{
+                        Color.parseColor(coloruse),
+                        Color.parseColor("#8EC1DD"),
+                }, null, Shader.TileMode.CLAMP);
+        temp.getPaint().setShader(textShader);
+        temp.setTextColor(Color.parseColor(coloruse));
+
+        city.setText(weatherdata.getString("citydata", "Bac Ninh"));
+
+
+
+        //Set image cache
+        switch(weatherdata.getString("statedata", "clear").toLowerCase()) {
+            case "clouds":
+                weather.setImageResource(R.drawable.clouds);
+                coloruse = "#8EC1DD";
+                state.setTextColor(Color.parseColor(coloruse));
+                break;
+            case "atmosphere":
+                weather.setImageResource(R.drawable.atmosphere);
+                coloruse = "#27B1FF";
+                state.setTextColor(Color.parseColor(coloruse));
+                break;
+            case "snow":
+                weather.setImageResource(R.drawable.snow);
+                coloruse = "#8EC1DD";
+                state.setTextColor(Color.parseColor(coloruse));
+                break;
+            case "rain":
+                weather.setImageResource(R.drawable.rain);
+                coloruse = "#4E8DB1";
+                state.setTextColor(Color.parseColor(coloruse));
+                break;
+            case "drizzle":
+                weather.setImageResource(R.drawable.rain);
+                coloruse = "#4E8DB1";
+                state.setTextColor(Color.parseColor(coloruse));
+                break;
+            case "thunderstorm":
+                weather.setImageResource(R.drawable.thunderstorm);
+                coloruse = "#BF8EDD";
+                state.setTextColor(Color.parseColor(coloruse));
+                break;
+            default:
+                weather.setImageResource(R.drawable.clear);
+                coloruse = "#FF8E27";
+                state.setTextColor(Color.parseColor(coloruse));
+                break;
+        }
+
+
+
+
 
 
 
@@ -78,22 +139,18 @@ public class MainActivity extends AppCompatActivity {
     public void getcityname(Location mLastLocation) {
         //Get the city name for openweatherapi
         String url = "https://api.openweathermap.org/geo/1.0/reverse?lat=" + mLastLocation.getLatitude() + "&lon=" + mLastLocation.getLongitude() + "&limit=1&appid=f6531adea60f0d386c77dce30be52c5e";
-        JsonArrayRequest JsonArrayRequest = new JsonArrayRequest(Request.Method.GET, url, null, new Response.Listener<JSONArray>() {
-            @Override
-            public void onResponse(JSONArray response) {
-                try {
-                    displayweather(response.getJSONObject(0).getString("name"));
-                    city.setText(response.getJSONObject(0).getString("name"));
-                } catch (JSONException e) {
-                    e.printStackTrace();
-                }
+        JsonArrayRequest JsonArrayRequest = new JsonArrayRequest(Request.Method.GET, url, null, response -> {
+            try {
+                displayweather(response.getJSONObject(0).getString("name"));
+                city.setText(response.getJSONObject(0).getString("name"));
+                editor = weatherdata.edit();
+                editor.putString("citydata", response.getJSONObject(0).getString("name"));
+                editor.apply();
+                //citydata
+            } catch (JSONException e) {
+                e.printStackTrace();
             }
-        }, new Response.ErrorListener() {
-            @Override
-            public void onErrorResponse(VolleyError error) {
-                Toast.makeText(MainActivity.this, error.getMessage(), Toast.LENGTH_SHORT).show();
-            }
-        });
+        }, error -> Toast.makeText(MainActivity.this, error.getMessage(), Toast.LENGTH_SHORT).show());
 
         requestQueue.add(JsonArrayRequest);
     }
@@ -102,81 +159,83 @@ public class MainActivity extends AppCompatActivity {
     public void displayweather(String cityname) {
         //Call with city name
         String url = "https://api.openweathermap.org/data/2.5/weather?q=" + cityname + "&units=metric&appid=f6531adea60f0d386c77dce30be52c5e";
-        JsonObjectRequest JsonObjectRequest = new JsonObjectRequest(Request.Method.GET, url, null, new Response.Listener<JSONObject>() {
-            @Override
-            public void onResponse(JSONObject response) {
-                try {
-                    //State
-                    state.setText(response.getJSONArray("weather").getJSONObject(0).getString("main"));
+        JsonObjectRequest JsonObjectRequest = new JsonObjectRequest(Request.Method.GET, url, null, response -> {
+            try {
+                //State
+                //statedata
+                state.setText(response.getJSONArray("weather").getJSONObject(0).getString("main"));
+                editor = weatherdata.edit();
+                editor.putString("statedata", response.getJSONArray("weather").getJSONObject(0).getString("main"));
 
-                    String checkstate = response.getJSONArray("weather").getJSONObject(0).getString("main");
 
-                    Log.d("Locationupdate", checkstate.toLowerCase());
-                    String coloruse = "#8EC1DD";
-                    switch(checkstate.toLowerCase()) {
-                        case "clouds":
-                            weather.setImageResource(R.drawable.clouds);
-                            coloruse = "#8EC1DD";
-                            state.setTextColor(Color.parseColor(coloruse));
-                            break;
-                        case "atmosphere":
-                            weather.setImageResource(R.drawable.atmosphere);
-                            coloruse = "#27B1FF";
-                            state.setTextColor(Color.parseColor(coloruse));
-                            break;
-                        case "snow":
-                            weather.setImageResource(R.drawable.snow);
-                            coloruse = "#8EC1DD";
-                            state.setTextColor(Color.parseColor(coloruse));
-                            break;
-                        case "rain":
-                            weather.setImageResource(R.drawable.rain);
-                            coloruse = "#4E8DB1";
-                            state.setTextColor(Color.parseColor(coloruse));
-                            break;
-                        case "drizzle":
-                            weather.setImageResource(R.drawable.rain);
-                            coloruse = "#4E8DB1";
-                            state.setTextColor(Color.parseColor(coloruse));
-                            break;
-                        case "thunderstorm":
-                            weather.setImageResource(R.drawable.thunderstorm);
-                            coloruse = "#BF8EDD";
-                            state.setTextColor(Color.parseColor(coloruse));
-                            break;
-                        default:
-                            weather.setImageResource(R.drawable.clear);
-                            coloruse = "#FF8E27";
-                            state.setTextColor(Color.parseColor(coloruse));
-                            break;
-                    }
 
-                    String temptext = (Math.round(Double.valueOf(response.getJSONObject("main").getString("temp")))) + "°";
-                    temp.setText(temptext);
-                    TextPaint paint = temp.getPaint();
-                    float width = paint.measureText(temptext);
 
-                    Shader textShader = new LinearGradient(10, 10, width, temp.getTextSize(),
-                            new int[]{
-                                    Color.parseColor(coloruse),
-                                    Color.parseColor("#ffffff"),
-                            }, null, Shader.TileMode.CLAMP);
-                    temp.getPaint().setShader(textShader);
-                    temp.setTextColor(Color.parseColor(coloruse));
-                    //Temperature
 
-                } catch (JSONException e) {
-                    e.printStackTrace();
+                String checkstate = response.getJSONArray("weather").getJSONObject(0).getString("main");
+                String coloruse;
+                switch(checkstate.toLowerCase()) {
+                    case "clouds":
+                        weather.setImageResource(R.drawable.clouds);
+
+                        coloruse = "#8EC1DD";
+                        state.setTextColor(Color.parseColor(coloruse));
+                        break;
+                    case "atmosphere":
+                        weather.setImageResource(R.drawable.atmosphere);
+                        coloruse = "#27B1FF";
+                        state.setTextColor(Color.parseColor(coloruse));
+                        break;
+                    case "snow":
+                        weather.setImageResource(R.drawable.snow);
+                        coloruse = "#8EC1DD";
+                        state.setTextColor(Color.parseColor(coloruse));
+                        break;
+                    case "rain":
+                        weather.setImageResource(R.drawable.rain);
+                        coloruse = "#4E8DB1";
+                        state.setTextColor(Color.parseColor(coloruse));
+                        break;
+                    case "drizzle":
+                        weather.setImageResource(R.drawable.rain);
+                        coloruse = "#4E8DB1";
+                        state.setTextColor(Color.parseColor(coloruse));
+                        break;
+                    case "thunderstorm":
+                        weather.setImageResource(R.drawable.thunderstorm);
+                        coloruse = "#BF8EDD";
+                        state.setTextColor(Color.parseColor(coloruse));
+                        break;
+                    default:
+                        weather.setImageResource(R.drawable.clear);
+                        coloruse = "#FF8E27";
+                        state.setTextColor(Color.parseColor(coloruse));
+                        break;
                 }
+                editor.putString("coloruse", coloruse);
+                //coloruse
+                String temptext = (Math.round(Double.parseDouble(response.getJSONObject("main").getString("temp")))) + "°";
+                temp.setText(temptext);
+                //tempdata
+                editor.putString("tempdata", String.valueOf((Math.round(Double.parseDouble(response.getJSONObject("main").getString("temp"))))));
+                editor.apply();
+                TextPaint paint = temp.getPaint();
+                float width = paint.measureText(temptext);
+
+                Shader textShader = new LinearGradient(10, 10, width, temp.getTextSize(),
+                        new int[]{
+                                Color.parseColor(coloruse),
+                                Color.parseColor("#ffffff"),
+                        }, null, Shader.TileMode.CLAMP);
+                temp.getPaint().setShader(textShader);
+                temp.setTextColor(Color.parseColor(coloruse));
+                //Temperature
+
+            } catch (JSONException e) {
+                e.printStackTrace();
+            }
 
 
-            }
-        }, new Response.ErrorListener() {
-            @Override
-            public void onErrorResponse(VolleyError error) {
-                Toast.makeText(MainActivity.this, error.getMessage(), Toast.LENGTH_SHORT).show();
-            }
-        });
+        }, error -> Toast.makeText(MainActivity.this, error.getMessage(), Toast.LENGTH_SHORT).show());
 
         requestQueue.add(JsonObjectRequest);
     }
@@ -189,14 +248,11 @@ public class MainActivity extends AppCompatActivity {
     private void getLocation() {
         //Get location and get city name
         if (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) { return; }
-        fusedLocationProviderClient.getLastLocation().addOnCompleteListener(new OnCompleteListener<Location>() {
-            @Override
-            public void onComplete(@NonNull Task<Location> task) {
-                Location location = task.getResult();
-                if (location != null) {
-                    getcityname(location);
+        fusedLocationProviderClient.getLastLocation().addOnCompleteListener(task -> {
+            Location location = task.getResult();
+            if (location != null) {
+                getcityname(location);
 
-                }
             }
         });
     }
@@ -216,7 +272,7 @@ public class MainActivity extends AppCompatActivity {
         return true;
     }
 
-    public void onRequestPermissionsResult(int requestCode, String[] permissions, int[] grantResults) {
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
         super.onRequestPermissionsResult(requestCode, permissions, grantResults);
         if (requestCode == LOCATION_PERMISSION && grantResults.length > 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
             getLocation();
